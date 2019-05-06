1d50c90f7af5bcc65521feaf53dbcb9193cf3a30
fospring <fospring@outlook.com>
2019-05-06 15:27:24 +0800

    modify examples/bxa/build.sh move all generated files to target folder

--- examples/bxa/build.sh
+++ examples/bxa/build.sh
@@ -2,8 +2,8 @@
 
 RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo +nightly build --release --target wasm32-unknown-unknown
 wasm-build --target=wasm32-unknown-unknown ../../target bxa
-wasm2wat ../../target/bxa.wasm -o ./bxa.wat
-wasm-gc ../../target/bxa.wasm ../../target/bxa.wasm
+wasm-gc ../../target/bxa.wasm ../../target/sample.wasm
+wasm2wat ../../target/sample.wasm -o ../../target/sample.wat
 #du -sh ../../target/*
 #wasm-gc ../../target/release/bxa.wasm ../../release/target/bxa.wasm
 #cargo rustc -- -Zunstable-options --pretty=expanded
