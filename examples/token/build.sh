d4e9139093e93631c1f628c36843a872bfe40c90
fospring <fospring@outlook.com>
2019-05-14 18:31:34 +0800

    modify address automaticly

--- examples/token/build.sh
+++ examples/token/build.sh
@@ -4,3 +4,4 @@ RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo +nightly build --release --targ
 wasm-build --target=wasm32-unknown-unknown ../../target token
 wasm-gc ../../target/token.wasm ../../examples/token/token.wasm
 wasm2wat ../../examples/token/token.wasm -o ./token.wat
+../../target/debug/bxa-gen-address ./token.wasm ./token.json
